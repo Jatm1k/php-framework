505f39467ed214859b9a809c5bf7d3c43d48d9c1
Jatmy <jatmyy@gmail.com>
2024-01-04 11:53:50 +0700

    routing

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,11 +2,32 @@
 
 namespace Jatmy\Framework\Http;
 
+use FastRoute\RouteCollector;
+
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
     public function handle(Request $request): Response
     {
-        $content = 'test';
-        return new Response($content);
+        $dispatcher = simpleDispatcher(function (RouteCollector $collector) {
+            $collector->get('/', function () {
+                $content = 'test';
+                return new Response($content);
+            });
+
+            $collector->get('/posts/{id}', function (array $vars) {
+                $content = "Post id {$vars['id']}";
+                return new Response($content);
+            });
+        });
+        $routeInfo = $dispatcher->dispatch(
+            $request->server['REQUEST_METHOD'],
+            $request->server['REQUEST_URI']
+        );
+        
+        [$status, $handler, $vars] = $routeInfo;
+
+        return $handler($vars);
     }
 }
