9c558e6dbc0d4d6b25f8b937ef0d7db7c40116c8
Jatmy <jatmyy@gmail.com>
2024-01-21 19:01:27 +0700

    session flash messages

---
+++ framework/src/Session/SessionInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Jatmy\Framework\Session;
+
+interface SessionInterface
+{
+    public function get(string $key, $default = null);
+
+    public function set(string $key, $value);
+
+    public function has(string $key): bool;
+
+    public function remove(string $key): void;
+
+    public function getFlash(string $type): array;
+
+    public function setFlash(string $type, string $message): void;
+
+    public function hasFlash(string $type): bool;
+
+    public function clearFlash(): void;
+}
